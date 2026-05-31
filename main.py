ceec9f0ad415729970079b4f0f6517fb86ec9381
yadhu krishna <yadhukris>
2026-05-31 15:06:07 +0530

    first comment

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://ai-interview-one-silk.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
